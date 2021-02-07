6428831f05bd4fad47b6075f5635ddf5d12e27ba
Kalam Mahardhika <kalammahardhika1002@gmail.com>
2021-02-07 16:54:53 +0700

    pertemuan 3

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+---------------------------
+Kalam Mahardhika
+---------------------------
+203040123
+---------------------------
+Teknik Informatika (D)
+---------------------------
+Minggu, 7 Februari 2021
+---------------------------
+Pemrograman Web
+---------------------------
+Pertemuan 3 - Struktur Kendali
+---------------------------
+*/
+
+?>
+
+<?php
+
+/*
+---------------------------
+Pengulangan
+for, while, do while, foreach(khusus array)
+---------------------------
+for( $i = 0; $i < 5; $i++ ) {
+    echo "Hello World! <br>";
+}
+---------------------------
+$i = 0;
+while( $i < 5 ) {
+    echo "Hello World! <br>";
+    $i++;
+}
+Ketika false tidak akan dijalankan
+---------------------------
+$i = 0;
+do {
+    echo "Hello World! <br>";
+    $i++;
+} while( $i < 5 );
+Ketika false akan dikerjakan minimal sekali
+---------------------------
+---------------------------
+---------------------------
+*/
+
+
+
+?>
