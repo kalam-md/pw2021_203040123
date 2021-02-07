60da5a5cf5e38961c180dce7bccdf81196bcc5a6
Kalam Mahardhika <kalammahardhika1002@gmail.com>
2021-02-07 13:38:20 +0700

    php dasar

---
+++ pertemuan1/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Kalam Mahardhika
+203040123
+Teknik Informatika (D)
+Minggu, 7 Februari 2021
+Pemrograman Web
+Menuliskan Hello World
+*/
+
+?>
+
+<?php
+
+echo "Hello World!";
+
+?>
---
+++ pertemuan2/index.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+---------------------------
+Kalam Mahardhika
+---------------------------
+203040123
+---------------------------
+Teknik Informatika (D)
+---------------------------
+Minggu, 7 Februari 2021
+---------------------------
+Pemrograman Web
+---------------------------
+Pertemuan 2 - Sintaks PHP
+---------------------------
+*/
+
+?>
+
+<?php
+
+/*
+---------------------------
+Standar Output:
+echo
+print
+print_r(cetak isi array) 
+var_dump(melihat isi variabel)
+---------------------------
+Penulisan sintaks PHP
+---------------------------
+1. PHP di dalam HTML
+2. HTML di dalam PHP
+---------------------------
+Variabel & Tipe Data
+Variabel tidak boleh diawali dengan angka, tapi boleh mengandung angka
+Variabel diawali dengan tanda $
+$nama = "Kalam"
+echo 'Halo, nama saya $nama;
+---------------------------
+Operator
+aritmatika (+ - / * %)
+$x = 10; 
+$y = 30;
+echo $x * $y;
+---------------------------
+Penggabung String / Concatenation / Concat
+Contohnya . operatornya
+$nama_depan = "Kalam";
+$nama_belakang = "Mahardhika;
+echo $nama_depan . " " . $nama_belakang;
+---------------------------
+Assignment
+Contohnya ( =, +=, -=, *=, /=, .= )
+$x = 1;
+$x += 5;
+echo $x;
+$nama = "Kalam"
+$nama .= " ";
+$nama .= "Kalam";
+echo $nama;
+---------------------------
+Perbandingan
+Contohnya ( <, >, <=, >=, ==, != )
+var_dump(1 < 5);
+var_dump(1 == "1");
+Tidak mengecek tipe data, hanya cek nilai
+---------------------------
+Identitas
+Contohnya (===, !==)
+var_dump(1 == "1");
+Cek tipe data dan nilainya
+---------------------------
+Logika
+&&(AND) jika salah satu true maka akan false, jika dua-duanya true akan true
+\\(OR) mau salah satu true hasilnya akan true
+!(NOT)
+$x = 10;
+var_dump($x < 20 && $x % 2 == 0);
+---------------------------
+*/
+
+?>
